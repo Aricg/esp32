77c8fae41defcb8d5c384d1864165c0e1a688e9f
Aric Gardner <aric.gardner@aifi.com>
2025-03-23 16:08:16 -0400

    feat: Add SGP41 gas sensor support with PlatformIO configuration

---
+++ esp32/ESP8266_SGP41_gas_sensor_new/src/main.cpp
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <SensirionI2CSgp41.h>
+#include <Wire.h>
+
+SensirionI2CSgp41 sgp41;
+
+// Time in seconds needed for NOx conditioning
+uint16_t conditioning_s = 10;
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(100);
+    }
+
+    Wire.begin();
+    sgp41.begin(Wire);
+
+    // Get and print serial number
+    uint16_t error;
+    char errorMessage[256];
+    uint16_t serialNumber[3];
+    error = sgp41.getSerialNumber(serialNumber);
+
+    if (error) {
+        Serial.print("Error trying to execute getSerialNumber(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.print("SerialNumber: 0x");
+        for (size_t i = 0; i < 3; i++) {
+            Serial.print(serialNumber[i], HEX);
+        }
+        Serial.println();
+    }
+
+    // Execute self test
+    uint16_t testResult;
+    error = sgp41.executeSelfTest(testResult);
+    if (error) {
+        Serial.print("Error trying to execute executeSelfTest(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else if (testResult != 0xD400) {
+        Serial.print("executeSelfTest failed with error: ");
+        Serial.println(testResult);
+    }
+}
+
+void loop() {
+    uint16_t error;
+    char errorMessage[256];
+    uint16_t defaultRh = 0x8000;  // 50% RH
+    uint16_t defaultT = 0x6666;   // 25°C
+    uint16_t srawVoc = 0;
+    uint16_t srawNox = 0;
+
+    delay(1000);
+
+    if (conditioning_s > 0) {
+        // During NOx conditioning (10s) SRAW NOx will remain 0
+        error = sgp41.executeConditioning(defaultRh, defaultT, srawVoc);
+        conditioning_s--;
+    } else {
+        // Read Measurement
+        error = sgp41.measureRawSignals(defaultRh, defaultT, srawVoc, srawNox);
+    }
+
+    if (error) {
+        Serial.print("Error trying to execute measureRawSignals(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.print("SRAW_VOC:");
+        Serial.print(srawVoc);
+        Serial.print("\t");
+        Serial.print("SRAW_NOx:");
+        Serial.println(srawNox);
+    }
+}
